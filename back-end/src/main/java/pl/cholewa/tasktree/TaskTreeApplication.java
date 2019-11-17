78df04e7b5927872f3a7258e13e093d73578a044
Krzysztof Cholewa (Home) <krzysztof.cholewa@gmail.com>
2019-11-17 20:34:28 +0100

    chore: back end initial commit

---
+++ back-end/src/main/java/pl/cholewa/tasktree/TaskTreeApplication.java
@@ -0,0 +1,13 @@
+package pl.cholewa.tasktree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTreeApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/pl/cholewa/tasktree/TaskTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cholewa.tasktree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
