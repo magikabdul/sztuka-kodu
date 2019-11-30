65613d79731b9ff174c6010c648b16667d256d9c
Krzysztof Cholewa (Home) <krzysztof.cholewa@gmail.com>
2019-12-01 00:03:41 +0100

    chore: AddNotePage

---
+++ front-end/src/layouts/AddNotePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header';
+
+const AddNotePage = () => {
+  return (
+    <>
+      <Header />
+      addNote
+    </>
+  );
+};
+
+export default AddNotePage;
